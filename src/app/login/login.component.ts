fa01be8d3addb9f0cf27795cd032e02c77914bd0
Donovan Triplett <donotriplett@gmail.com>
2018-11-28 10:57:09 -0500

    styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -27,5 +27,4 @@ export class LoginComponent implements OnInit {
   }
   this._login.signIn(this.loginForm.value).subscribe((res: any) => {this.token = res.token; localStorage.setItem("token", this.token)})
   }
-
 }
